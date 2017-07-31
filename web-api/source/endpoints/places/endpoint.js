11265ab367bf20b503e624466839b4c9e56acec8
Francisco Mercedes <E1003102@ercbpo.com>
2017-07-31 14:27:36 -0400

    Creating new places endpoint operations, adding mongo.

--- web-api/source/endpoints/places/endpoint.js
+++ web-api/source/endpoints/places/endpoint.js
@@ -3,12 +3,40 @@
 module.exports = (function () {
   var privateScope = new WeakMap()
 
+  /*, placeRatingService*/
   function PlacesEndpoint (placesSearchService) {
-    privateScope.set(this, placesSearchService)
-  };
+    privateScope.set(this, {
+      //placeRatingService: placeRatingService,
+      placesSearchService: placesSearchService
+    })
+  }
+
+  PlacesEndpoint.prototype.getPlacesTypes = function (request, reply) {
+    let placesSearchService = privateScope.get(this).placesSearchService
+
+    placesSearchService.getPlacesLocatedArround(request.query)
+      .then(places => reply(places))
+      .catch(error => reply(error))
+  }
 
   PlacesEndpoint.prototype.getPlacesLocatedArround = function (request, reply) {
-    let placesSearchService = privateScope.get(this)
+    let placesSearchService = privateScope.get(this).placesSearchService
+
+    placesSearchService.getPlacesLocatedArround(request.query)
+      .then(places => reply(places))
+      .catch(error => reply(error))
+  }
+
+  PlacesEndpoint.prototype.getPlacesInformation = function (request, reply) {
+    let placesSearchService = privateScope.get(this).placesSearchService
+
+    placesSearchService.getPlacesLocatedArround(request.query)
+      .then(places => reply(places))
+      .catch(error => reply(error))
+  }
+
+  PlacesEndpoint.prototype.ratePlace = function (request, reply) {
+    let placesSearchService = privateScope.get(this).placesSearchService
 
     placesSearchService.getPlacesLocatedArround(request.query)
       .then(places => reply(places))
--- web-api/source/endpoints/places/router.js
+++ web-api/source/endpoints/places/router.js
@@ -5,14 +5,28 @@ module.exports = (function () {
 
   function PlacesRouter (placesEndpoint) {
     privateScope.set(this, placesEndpoint)
-  };
+  }
 
   PlacesRouter.prototype.route = function (hapiServer) {
     var placesEndpoint = privateScope.get(this)
 
+    // TODO: explain why we need to bind here.
+
     hapiServer.route({ method: 'GET',
       path: '/places',
-      handler: placesEndpoint.getPlacesLocatedArround.bind(placesEndpoint) }) // TODO: explain why we need to bind here. 
+      handler: placesEndpoint.getPlacesLocatedArround.bind(placesEndpoint) })  
+    
+    hapiServer.route({ method: 'GET',
+      path: '/places/types',
+      handler: placesEndpoint.getPlacesTypes.bind(placesEndpoint) })
+
+    hapiServer.route({ method: 'GET',
+      path: '/places/information/{providerIdentifier}/{placeIdentifier}',
+      handler: placesEndpoint.getPlacesInformation.bind(placesEndpoint) })
+
+    hapiServer.route({ method: 'POST',
+      path: '/places/rate',
+      handler: placesEndpoint.ratePlace.bind(placesEndpoint) })
   }
 
   return PlacesRouter

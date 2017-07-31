76806028701f330bc5582fd670843e610bc539e2
Francisco Mercedes <E1003102@ercbpo.com>
2017-07-30 22:57:20 -0400

    Start writing test and configuring task with gulp.

--- web-api/gulpfile.js
+++ web-api/gulpfile.js
@@ -2,9 +2,22 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
  
-gulp.task('default', function () {
+gulp.task('default', ['test']);
+
+/* ==================================================== 
+      The lint task. [code integrity and standar]          
+   ==================================================== */
+gulp.task('lint', () => {
     return gulp.src(['./source/**/*.js'])
             .pipe(eslint())
             .pipe(eslint.format()) 
             .pipe(eslint.failAfterError());
 });
+
+/* ==================================================== 
+      The test task. [unit and integration testing]          
+   ==================================================== */
+gulp.task('test', ['lint'], () =>
+    gulp.src('./source/**/*spec.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+);
